e5f9078a48f133582f2e376e2bd3b4b689e30d9a
makotia <3mdevmail@gmail.com>
2020-11-10 14:44:34 +0900

    :+1: サブディレクトリ

--- cli/fix-export.js
+++ cli/fix-export.js
@@ -3,15 +3,35 @@ var path = require('path')
 
 const outDirPath = path.join(__filename, '../../out')
 
-fs.readdir(outDirPath, function(_, files){
-  const f = files.filter(file => file.match(/.*\.html$/))
-  f.forEach(file => {
-    const filePath = path.join(outDirPath, file)
-    let text = fs.readFileSync(filePath).toString('utf-8')
-    text = text.replace(new RegExp('src="/', 'g'), 'src="')
-    text = text.replace(new RegExp('href="/', 'g'), 'href="')
-    fs.writeFileSync(filePath, text, (err) => {
-      if (err) console.log(err)
-    })
+const readdirRecursively = (dir, files = []) => {
+  const paths = fs.readdirSync(dir)
+  const dirs = []
+  for (const path of paths) {
+    const stats = fs.statSync(`${dir}/${path}`)
+    if (stats.isDirectory()) {
+      dirs.push(`${dir}/${path}`)
+    } else {
+      files.push(`${dir}/${path}`)
+    }
+  }
+  for (const d of dirs) {
+    files = readdirRecursively(d, files)
+  }
+  return files.filter(file => file.match(/.*\.html$/))
+}
+
+const replaceText = (filePath) => {
+  let relative = path.relative(path.dirname(filePath), outDirPath)
+  relative = (relative != '' ? `${relative}/` : relative)
+  let text = fs.readFileSync(filePath).toString('utf-8')
+  text = text.replace(new RegExp('src="/', 'g'), `src="${relative}`)
+  text = text.replace(new RegExp('href="/', 'g'), `href="${relative}`)
+  fs.writeFileSync(filePath, text, (err) => {
+    if (err) console.log(err)
   })
+}
+
+const f = readdirRecursively(outDirPath)
+f.forEach(file => {
+  replaceText(file)
 })
