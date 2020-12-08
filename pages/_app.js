5379b4a2f586c7648d723f6fca7a0e70df9c22a6
datt <osatom.4639@gmail.com>
2020-12-09 08:28:47 +0900

    :recycle: componentの拡張子をjsxにした

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import "../styles/globals.css";
-import Header from "../components/header.js";
+import Header from "../components/header.jsx";
 
 function MyApp({ Component, pageProps }) {
   return (
