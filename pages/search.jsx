1aec87ddc06351e820b223df9c00fa1aea5a5b41
makotia <3mdevmail@gmail.com>
2020-12-09 16:42:48 +0900

    :+1: 三項演算子をマシに

--- pages/search.jsx
+++ pages/search.jsx
@@ -1,16 +1,17 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import ItemCard from "../components/itemCard";
-import data from "../lib/articles";
 import { useRouter } from 'next/router'
 import { searchAllTags, searchAnyTags } from '../lib/search'
 
 export default function Home() {
   const router = useRouter()
-  const { type, tags, mode } = router.query
+  let { type, tags, mode } = router.query
+  tags = tags ? tags.split(',') : []
+  type = type ? type : ''
   const list = !mode ? []
-            : (mode === 'and') ? searchAllTags(tags ? tags.split(',') : [], type ? type : '')
-            : searchAnyTags(tags ? tags.split(',') : [], type ? type : '')
+            : (mode === 'and') ? searchAllTags(tags, type)
+            : searchAnyTags(tags, type)
   return (
     <div className={styles.container}>
       <Head>
