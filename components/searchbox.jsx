1428e43d2c6d13043baf065b9e32f031fb2b255c
datt <osatom.4639@gmail.com>
2020-12-09 21:39:20 +0900

    :art: 検索アイコンだけにした

--- components/searchbox.jsx
+++ components/searchbox.jsx
@@ -1,21 +1,12 @@
 import styles from "../styles/SearchBox.module.css";
+import { AiOutlineSearch } from "react-icons/ai";
 
-// TODO: id, nameの設定
 export default function SerachBox() {
   return (
     <div>
-      <form id="***" action="#" method="get" className={styles.form2}>
-        <input
-          className={styles.sbox2}
-          id="***" // TODO: 後で追記
-          name="***" // TODO: 後で追記
-          type="text"
-          placeholder="フリーワードを入力"
-        />
-        {/* TODO: idの設定 */}
-        <button type="submit" id="***" className={styles.sbtn2}>
-          <span>&gt;</span>
-          {/* TODO: 検索アイコンどにかする */}
+      <form id="sform" action="#" method="get" className={styles.form2}>
+        <button type="submit" id="sbtn" className={styles.sbtn2}>
+          <AiOutlineSearch />
         </button>
       </form>
     </div>
