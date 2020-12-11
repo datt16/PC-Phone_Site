72a10c8e53a5aea3bc119d68f7c773c5a7377973
makotia <3mdevmail@gmail.com>
2020-12-11 14:43:29 +0900

    :+1: 生成

--- src/lib/articles.js
+++ src/lib/articles.js
@@ -1,24 +1,11 @@
 const data = [
   {
-    id: 'iPhone-X',
-    name: 'iPhone X',
-    image: '/iPhone-X.jpg',
-    type: 'mobile',
-    tags: ['Apple', 'iPhone', 'iOS', 'Previous'],
-  },
-  {
-    id: 'iPad-Pro',
-    name: 'iPad Pro',
-    image: '/iPad-Pro.jpg',
-    type: 'mobile',
-    tags: ['Apple', 'iPad', 'iOS', 'Latest'],
-  },
-  {
     id: 'MacBook-Pro',
     name: 'MacBook Pro',
     image: '/MacBook-Pro.jpg',
     type: 'pc',
     tags: ['Apple', 'Mac', 'macOS', 'Latest'],
+    content: '# MacBook Pro\n\nMacBook Pro は，Apple社製のラップトップ型コンピュータです．\n',
   },
   {
     id: 'XPS-13',
@@ -26,6 +13,23 @@ const data = [
     image: '/XPS-13.jpg',
     type: 'pc',
     tags: ['Dell', 'Windows', 'Latest'],
+    content: '# XPS 13\n\nXPSは、Dell（デル）のパーソナルコンピュータの一つである。 eXtreme Performance Systemの略。 \n\n元々は、デスクトップ向けのDimensionシリーズの上位機種として発売され、後にノートブック向けのInspironシリーズの上位機種として投入される。今後は「Alienware」が後継シリーズとして投入する事が決定された為にXPSシリーズはミドルレンジのデスクトップとして存続していく方向である。 \n',
+  },
+  {
+    id: 'iPad-Pro',
+    name: 'iPad Pro',
+    image: '/iPad-Pro.jpg',
+    type: 'pc',
+    tags: ['Apple', 'iPad', 'iOS', 'Latest'],
+    content: '# iPad Pro\n\n**iPad Pro**（アイパッド プロ）はアップルが開発、販売するタブレット型コンピュータである。iPadシリーズにおける最上位モデルとなっている。 \n\n### 2015年\n- 9月9日（現地時間）、アメリカ・カリフォルニア州サンフランシスコのBill Graham Civic Auditoriumで開催された「Special Event September 2015」で12.9インチ版を発表、同年11月11日に発売された\n\n### 2016年\n- 3月21日（現地時間）、スペシャルイベントの基調講演で、9.7インチ版が追加発表された[3]。同日、12.9インチ版の256GBモデルが追加された。\n\n### 2017年\n- 6月5日（現地時間）、WWDC 2017で12.9インチ版の第2世代と10.5インチ版が追加発表され、9.7インチ版の販売が終了された。\n\n### 2018年\n- 10月30日（現地時間）、スペシャルイベントで、狭額ベゼルのLiquid Retinaディスプレイを搭載し、ホームボタンが廃止されFace IDを搭載した11インチ版と12.9インチ版とそれら専用の新しいApple Pencil（第2世代）とSmart Keyboard Folioが発表された。\n\n### 2020年\n- 3月18日（現地時間）、A12XからGPUを一つ増やしたA12Z Bionic、2つの広角カメラとToF方式のLiDARスキャナを搭載した11インチと12.9インチのiPad Proが、Multi-Touchトラックパッドが搭載されたMagic Keyboardと共に発表された[4]。\n',
+  },
+  {
+    id: 'iPhone-X',
+    name: 'iPhone X',
+    image: '/iPhone X.jpg',
+    type: 'mobile',
+    tags: ['Apple', 'iPhone', 'iOS', 'Previous'],
+    content: '# iPhone X\n\n**iPhone X**（アイフォーン テン）は、アップルが開発・販売していたスマートフォンである。\n\niPhone Xは、2017年9月12日（現地時間）、アメリカ・カリフォルニア州クパティーノのSteve Jobs Theaterで初開催されたアップルのスペシャルイベントの最後「One more thing...」で発表された。\n\n2017年10月27日から予約開始。耐水・防塵はIP67等級を有している。\n\niPhone Xの外観は前面がOLEDディスプレイで覆われており、背面がガラスパネルになったことでワイヤレス給電規格のQiに対応した。これによりQi対応のワイヤレス充電器や、アップルから発売予定のAirPowerによりワイヤレス充電が可能になる。しかし、2019年3月29日(現地時間)に、AirPowerの開発中止が発表された。iPhone 4s以前で採用されていた、ステンレス製のフレームを採用している。頑丈で 、アルミニウムフレームにはない光沢が得られている。ステンレスのカラーはシルバーの一色だけである。CPUにはApple A11 Bionicが採用。Face IDと呼ばれる顔だけでロックを解除することができる新たな認証システムを採用。さらにアニ文字と呼ばれる機能が搭載されている。これらの機能は新しく採用されたTrueDepthカメラによって実現されているため、iPhone X、iPhone XS、XS Max、XRのみで利用できる。\n\n2018年9月13日のiPhone XS、XS Max、XRの発表と同時に公式ホームページからの販売は終了したが、アップル取扱店では販売を継続している。 \n',
   },
 ]
 
