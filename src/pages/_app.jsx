42ec31602e2fb3b3cfe687dc1c9b2226d77ebac4
makotia <3mdevmail@gmail.com>
2020-12-16 11:01:53 +0900

    :art: フッターの位置を修正

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import '../styles/globals.css'
+import styles from '../styles/_app.module.css'
 import Header from '../components/header.jsx'
 import Footer from '../components/footer.jsx'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div>
+    <div className={styles.wrapper}>
       <Header />
       <Component {...pageProps} />
       <Footer />
