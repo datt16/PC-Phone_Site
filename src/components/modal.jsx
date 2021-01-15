c4655c009f354bbf2887b92373eb0b49cbbb945f
datt16 <osatom.4639@gmail.com>
2021-01-15 21:26:43 +0900

    :art: アニメーションを無効化

--- src/components/modal.jsx
+++ src/components/modal.jsx
@@ -5,15 +5,15 @@ export default function Modal(props) {
   const [isOpen, setOpen] = useState(props.open)
   const hide = () => {
     props.handleClose()
-    console.log('toHide')
     setOpen(false)
   }
+
   return (
     <div
       onClick={hide}
-      className={`${isOpen ? styles.SHOW : styles.HIDE} + ${styles.wrapper}`}
+      className={`${styles.wrapper} + ${isOpen ? styles.SHOW : styles.HIDE}`}
     >
       <div className={styles.frame}>{props.children}</div>
     </div>
-  )
+  );
 }
