e53741ba7f4384a70ee2e31119a2e6e6f266bcb3
datt16 <osatom.4639@gmail.com>
2021-01-21 16:03:46 +0900

    :art: Markdown部分のデザインを仮組

--- src/pages/device/[id].jsx
+++ src/pages/device/[id].jsx
@@ -62,7 +62,7 @@ export default function Home() {
                   weight={article.weight}
                 />
               </div>
-              <ReactMarkdown>{article.content}</ReactMarkdown>
+              <ReactMarkdown className={styles.markdown}>{article.content}</ReactMarkdown>
             </article>
           </main>
         </>
