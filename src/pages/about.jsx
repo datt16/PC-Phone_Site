918e06cbdfaddaf4e08c58bd0e7f1b47a8048339
datt16 <osatom.4639@gmail.com>
2021-01-25 20:22:39 +0900

    :art: デザイン調整

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,18 +1,33 @@
 import Head from 'next/head'
+import Link from 'next/link'
 import React from 'react'
-import styles from '../styles/Home.module.css'
+import base from '../styles/Home.module.css'
+import styles from '../styles/About.module.css'
+import { FaGithub } from 'react-icons/fa'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className={base.container}>
       <Head>
         <title>Search | PC Phone Site</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <div className={styles.form}>
+      <main className={base.main}>
+        <div className={base.form}>
           <label>ABOUT</label>
+        </div>
+        <div className={styles.container}>
+          <h2>Who makes this site?</h2>
+          <p>2021年度 情報工学基礎実習II 6グループ</p>
+
+          <h2>Repository</h2>
+          <Link href="https://github.com/datt16/PC-Phone_Site">
+            <div className={styles.githubLinkArea}>
+              <FaGithub size="3em" />
+              <a className={styles.githubLink}>GitHub</a>
+            </div>
+          </Link>
         </div>
       </main>
     </div>
