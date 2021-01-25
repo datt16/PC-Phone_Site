94df77dd56f9930961591d72cdd247a650a5a11f
makotia <3mdevmail@gmail.com>
2021-01-26 01:56:57 +0900

    :art: 迷ったら中央

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -4,31 +4,44 @@ import React from 'react'
 import base from '../styles/Home.module.css'
 import styles from '../styles/About.module.css'
 import { FaGithub } from 'react-icons/fa'
+import { RiUnsplashFill } from 'react-icons/ri'
+import { IoLogoVercel } from 'react-icons/io5'
 
 export default function Home() {
   return (
-    <div className={base.container}>
+    <div className={styles.container}>
       <Head>
-        <title>Search | PC Phone Site</title>
+        <title>About | PC Phone Site</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={base.main}>
-        <div className={base.form}>
-          <label>ABOUT</label>
-        </div>
-        <div className={styles.container}>
-          <h2>Who makes this site?</h2>
-          <p>2021年度 情報工学基礎実習II 6グループ</p>
+      <main className={styles.main}>
+        <h2>Made by</h2>
+        <p>2021年度 情報工学基礎実習II 6グループ</p>
 
-          <h2>Repository</h2>
-          <Link href="https://github.com/datt16/PC-Phone_Site">
-            <div className={styles.githubLinkArea}>
-              <FaGithub size="3em" />
-              <a className={styles.githubLink}>GitHub</a>
-            </div>
-          </Link>
-        </div>
+        <h2>Code Hosted by</h2>
+        <Link href="https://github.com/datt16/PC-Phone_Site">
+          <div className={styles.siteArea}>
+            <FaGithub size="2.5em" />
+            <a className={styles.githubLink}>GitHub</a>
+          </div>
+        </Link>
+
+        <h2>Image by</h2>
+        <Link href="https://unsplash.com">
+          <div className={styles.siteArea}>
+            <RiUnsplashFill size="2.5em" />
+            <a>Unsplash</a>
+          </div>
+        </Link>
+
+        <h2>Code Hosted by</h2>
+        <Link href="https://github.com/datt16/PC-Phone_Site">
+          <div className={styles.siteArea}>
+            <IoLogoVercel size="2.5em" />
+            <a>Vercel</a>
+          </div>
+        </Link>
       </main>
     </div>
   )
